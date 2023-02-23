1ab049d572bb470b9caf7d4b8912e11884b8d511
TheWisePigeon <zozozozeph@gmail.com>
2023-02-23 23:50:17 +0000

    added json

--- projet1/index.js
+++ projet1/index.js
@@ -1,1 +1,12 @@
-console.log("Hello bozo")
+const personne = {
+    nom:"",
+    age:10
+}
+
+const stringg = JSON.stringify(personne)
+console.log(stringg)
+//envoie par http
+{
+    const personne = JSON.parse(stringg)
+    console.log(personne)
+}
